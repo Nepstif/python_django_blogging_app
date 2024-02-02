c39f725fbff004e27d155b6b717fd54600513d11
Ratmir Ishmuratov <nepstif@gmail.com>
2024-02-03 00:51:14 +0500

    templates developed blog

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404 as get_object
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  "blog/post/list.html",
+                  {"posts": posts})
+
+
+def post_detail(request, id):
+    post = get_object(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, "blog/post/detail.html",
+                  {"post": post})
