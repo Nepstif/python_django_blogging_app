2c29ec76262c26c68e95d73dd0bb51aa69d32f0d
Ratmir Ishmuratov <nepstif@gmail.com>
2024-02-05 21:41:43 +0500

    added Tags

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,13 +3,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404 as get_object
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post, Comment
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
+    print(tag_slug)
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -24,7 +30,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   "blog/post/list.html",
-                  {"posts": posts})
+                  {"posts": posts,
+                   "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
